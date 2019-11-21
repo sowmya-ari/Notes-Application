ae7d2e78a59a3544e52eb8a9dc0d5e4771a0e68c
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-11-21 15:50:34 +0530

    Notes model is modified

---
+++ server/models/Notes.js
@@ -0,0 +1,20 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Notes = sequelize.define(
+    "Notes",
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      color: DataTypes.VARCHAR
+    },
+    {}
+  );
+  Notes.associate = function(models) {
+    // associations can be defined here
+  };
+  return Notes;
+};
