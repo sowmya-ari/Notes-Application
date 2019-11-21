91dd5a0633be87d987836ab3470e8dc3e2a1ffd6
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-11-21 14:00:47 +0530

    Index file of models folder is modified

--- server/models/Index.js
+++ server/models/Index.js
@@ -1,22 +1,41 @@
 "use strict";
-
+const fs = require("fs");
+const path = require("path");
+const basename = path.basename(__filename);
+const dotenv = require("dotenv");
 const Sequelize = require("sequelize");
-const env = require("./env");
+dotenv.config();
 const sequelize = new Sequelize(
-  env.POSTGRES_DATABASE,
-  env.POSTGRES_USERNAME,
-  env.POSTGRES_PASSWORD,
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USERNAME,
+  process.env.POSTGRES_PASSWORD,
   {
-    host: env.POSTGRES_HOST,
-    port: env.POSTGRES_PORT,
-    dialect: env.POSTGRES_DIALECT,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: "postgres",
     define: {
       underscored: true
     }
   }
 );
-
+console.log(sequelize);
 const db = {};
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach(file => {
+    const model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
