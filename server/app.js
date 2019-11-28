a2c431831ffe542187c00bdf59c3a02cc0f2f58c
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-11-28 16:22:50 +0530

    App.js is modified and added middlewares like cors and jwt token validation

--- server/app.js
+++ server/app.js
@@ -1,22 +1,31 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const models = require("./models/Index");
-const app = express();
+const notesroutes = require("./routes/Notes");
+const userroutes = require("./routes/Users");
+const validatetoken = require("./utils/Jwt");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(userroutes);
+app.use(validatetoken.ValidateToken);
+app.use(notesroutes);
 
 app.get("/", (req, res) => {
   res.json({
-    message:
-      "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."
+    message: "Welcome to Notes application"
   });
 });
 dotenv.config();
 const port = process.env.PORT;
-models.sequelize.sync({ force: true }).then(() => {
+models.sequelize.sync().then(() => {
   app.listen(port, () => {
-    console.log(`notes application is listening on port ${port}!`);
+    console.log(`Notes application is listening on port ${port}!`);
   });
 });
+
+module.exports = app;
