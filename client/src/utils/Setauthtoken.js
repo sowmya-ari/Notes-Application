08b5fec7f148376e9c6d7fa429e014e2a315f26a
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-12-16 05:45:36 +0530

    File is deleted

--- client/src/utils/Setauthtoken.js
+++
@@ -1,7 +0,0 @@
-var token = localStorage.getItem("Token");
-var user_id = localStorage.getItem("user_id");
-const Setauthtoken = function() {
-  const result = { token, user_id };
-  return result;
-};
-export default Setauthtoken;
