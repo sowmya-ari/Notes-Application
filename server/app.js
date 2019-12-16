341390620825b82bcaf7c55b5b1b7474783f3cfc
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-12-16 05:45:06 +0530

    Unwanted spaces are removed

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const models = require("./models/Index");
-const notesroutes = require("./routes/Notes");
-const userroutes = require("./routes/Users");
-const validatetoken = require("./utils/Jwt");
+const notesRoutes = require("./routes/Notes");
+const userRoutes = require("./routes/Users");
+const validateToken = require("./utils/Jwt");
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -17,9 +17,9 @@ app.get("/", (req, res) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(userroutes);
-app.use(validatetoken.ValidateToken);
-app.use(notesroutes);
+app.use(userRoutes);
+app.use(validateToken.ValidateToken);
+app.use(notesRoutes);
 
 dotenv.config();
 const port = process.env.PORT;
